3e9dd8628157f4c3d8bc72a55a48fa73eabefe4d
Pabodha Wanniarachchi <pabodhawanniarachchi@gmail.com>
2026-06-29 17:04:45 +0530

    feat: Installed and configured Global Toasts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import useAuthStore from './store/authStore';
 import ProtectedRoute from './routing/ProtectedRoute';
 
@@ -14,7 +16,7 @@ import SelectRolePage from './pages/SelectRolePage';
 import HomePage from './pages/HomePage';
 import CreateProjectPage from './pages/CreateProjectPage';
 // import ProjectDetailPage from './pages/ProjectDetailPage';
-// import ProfilePage from './pages/ProfilePage';
+import ProfilePage from './pages/ProfilePage';
 // import AdminPage from './pages/AdminPage';
 
 function App() {
@@ -23,7 +25,9 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
+    <>
+      <ToastContainer position="bottom-right" autoClose={4000} hideProgressBar={true} theme="colored" />
+      <BrowserRouter>
       <Routes>
         {/* Public Routes */}
         <Route path="/login" element={<LoginPage />} />
@@ -65,14 +69,14 @@ function App() {
             }
           /> */}
           {/* <Route path="/projects/:id" element={<ProjectDetailPage />} /> */}
-          {/* <Route
+          <Route
             path="/my-portfolio"
             element={
               <ProtectedRoute allowedRoles={['STUDENT']}>
                 <ProfilePage />
               </ProtectedRoute>
             }
-          /> */}
+          />
           {/* <Route 
             path="/admin" 
             element={
@@ -87,6 +91,7 @@ function App() {
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
+    </>
   );
 }
 
